e2eccba77dcea0c448794f699c261bda59a314ef
Badilld <78575189+Badilld@users.noreply.github.com>
2023-05-17 09:32:56 -0700

    Create concat.ts

---
+++ 1929-concatenation-of-array/concat.ts
@@ -0,0 +1,10 @@
+/** https://leetcode.com/problems/create-hello-world-function/
+* concat.ts
+* By: Badilld
+* Date: 5-17-23
+* Description: Concatenate 2 arrays, why is this a question?
+*/
+
+function getConcatenation(nums: number[]): number[] {
+    return nums.concat(nums);
+};
