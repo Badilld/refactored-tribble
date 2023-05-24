8c38afc3b80422838e5d981727772c02d8a0ef73
Badilld <78575189+Badilld@users.noreply.github.com>
2023-05-24 14:17:14 -0700

    Create arrReduce.ts

---
+++ 2626-array-reduce-transform/arrReduce.ts
@@ -0,0 +1,21 @@
+/** https://leetcode.com/problems/array-reduce-transformation/
+* arrReduce.ts.ts
+* By: Badilld
+* Date: 5-24-23
+* Description: Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+
+var reduce = function(nums, fn, init) {
+    let val = init;
+    nums.forEach( num => {
+        val = fn(val, num);
+    });
+    return val;
+};
